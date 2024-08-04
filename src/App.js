e2704002d4d3db6d3fee7bfc000386a7699460b4
Techgirlie-martinermars <131309536+Techgirlie-martinermars@users.noreply.github.com>
2024-08-04 22:48:12 +0300

    added a url to github and netlify

--- src/App.js
+++ src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      weather app
+      <footer>
+        This project was coded by Martiner Winceit and is {""}
+        <a
+          href="https://github.com/Techgirlie-martinermars/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+        {""} and hosted on
+        <a
+          href="https://incredible-chebakia-5404ce.netlify.app/"
+          target="_blank"
+        >
+          {""}
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
