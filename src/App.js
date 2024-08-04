7bf4a6a920570579a3dd67e4b2af3cbca78fcef2
Techgirlie-martinermars <131309536+Techgirlie-martinermars@users.noreply.github.com>
2024-08-05 00:31:29 +0300

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      weather app
-      <footer>
-        This project was coded by Martiner Winceit and is {""}
-        <a
-          href="https://github.com/Techgirlie-martinermars/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-        {""} and hosted on
-        <a
-          href="https://incredible-chebakia-5404ce.netlify.app/"
-          target="_blank"
-        >
-          {""}
-          Netlify
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>weather app</h1>
+        <footer>
+          This project was coded by Martiner Winceit and is {""}
+          <a
+            href="https://github.com/Techgirlie-martinermars/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+          {""} and hosted on
+          <a
+            href="https://incredible-chebakia-5404ce.netlify.app/"
+            target="_blank"
+          >
+            {""}
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
