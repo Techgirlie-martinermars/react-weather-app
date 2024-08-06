b7ecb6bf81a5ec9d4ae762067219af5d5d5dc467
Techgirlie-martinermars <131309536+Techgirlie-martinermars@users.noreply.github.com>
2024-08-06 23:48:27 +0300

    added "norefferrer"

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default function App() {
           <a
             href="https://incredible-chebakia-5404ce.netlify.app/"
             target="_blank"
+            rel="noreferrer"
           >
             {""}
             Netlify
